c93711a5bb6df5642aa743c1ffee3444127574d4
todorov3 <125344185+todorov3@users.noreply.github.com>
2025-05-21 23:51:04 +0300

    Update class Booking

--- src/Booking.java
+++ src/Booking.java
@@ -1,6 +1,7 @@
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Booking {
     private String username;
@@ -14,6 +15,7 @@ public class Booking {
         this.roomNumber = roomNumber;
         this.checkIn = LocalDate.parse(checkIn, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         this.checkOut = LocalDate.parse(checkOut, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        this.totalPrice = totalPrice;
+        long nights = ChronoUnit.DAYS.between(this.checkIn, this.checkOut);
+        this.totalPrice = pricePerNight.multiply(BigDecimal.valueOf(nights));
     }
 }
